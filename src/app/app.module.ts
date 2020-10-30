cd1103e81452911f6a8af6dc2b871fdef40fa63a
Jakub Nowak <nowakjakub095@gmail.com>
2020-10-30 08:55:06 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Loginpage } from './components/loginpage/loginpage.component/loginpage.component.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Loginpage.ComponentComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
