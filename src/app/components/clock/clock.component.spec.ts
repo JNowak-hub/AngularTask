3736ceaa92a5b3154fc88c8cfe983aae2609f4d8
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-03 17:43:23 +0100

    added tests for clock component

--- src/app/components/clock/clock.component.spec.ts
+++ src/app/components/clock/clock.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClockComponent } from './clock.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('ClockComponent', () => {
   let component: ClockComponent;
   let fixture: ComponentFixture<ClockComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +19,22 @@ describe('ClockComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ClockComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should write correct time in element', () => {
+    const regex = new RegExp(/^(?:0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/);
+    expect(de.query(By.css('div')).nativeElement.innerText).toMatch(regex);
+  });
+
+
+  it('function should write correct time', () => {
+    const regex = new RegExp(/^(?:0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/);
+    expect(component.writePrettieTime()).toMatch(regex);
   });
 });
